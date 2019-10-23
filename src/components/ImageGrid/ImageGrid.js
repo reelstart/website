0ea036ec7c9a709ce1363be197a2d34040683039
Pramesh Attwala <cheers@prameshattwala.com>
2019-10-23 10:06:53 -0400

    Start of IG feed integration. Got personal feed hooked up. Need to get @reelstartorg hooked in.

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { buildUrl } from 'react-instafeed'
+ 
 
 //image import
 import { gridImages } from '../../data/gridImages';
 
 //image modal
 import ImageModal from '../ImageModal/ImageModal';
+
+const options = {
+  accessToken: '41365.d90570a.6896b731b4c2478dbe8cd64cbefccb4d',
+  clientId: '1051502a682b424385b1d0ee17180955',
+  get: 'user', // popular, user
+  locationId: null,
+  resolution: 'standard_resolution', // thumbnail, low_resolution, standard_resolution
+  sortBy: 'most-recent', // none, least-commented, least-liked, least-recent, most-commented, most-liked, most-recent, random
+  tagName: null,
+  userId: '41365',
+};
+
+console.log(options);
+
+const instagramUrl = buildUrl(options);
+
+let instaData;
+
 
 class ImageGrid extends Component {
 	state = {
@@ -20,28 +40,55 @@ class ImageGrid extends Component {
 		this.setState({ modalShow: false });
 	};
 
+	componentDidMount() {
+	    fetch(instagramUrl)
+			.then(res => res.json())
+			.then((result) => {
+				this.setState({
+					isLoaded: true,
+					instaData: result.data.slice(10,18)
+				});
+			},
+			(error) => {
+				this.setState({
+					isLoaded: true,
+					error
+				});
+	        }
+			);
+  	}
+
 	render() {
-		return (
-			<div id="gallery" className="ImageGridSection">
-				{this.state.modalShow && <ImageModal
-					onHide={this.modalClose}
-					show={this.state.modalShow}
-					images={gridImages}
-					imageid={this.state.imageId}
-				/>}
-				{gridImages.map(image => {
-					return (
-						<img
-							src={image.image}
-							alt="image on set"
-							className="grid-image"
-							onClick={() => this.handleShow(image.id)}
-							key={image.id}
-						/>
-					);
-				})}
-			</div>
-		);
+		const { error, isLoaded, instaData } = this.state;
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div id="gallery" className="ImageGridSection">
+					{/*this.state.modalShow && <ImageModal
+					 	onHide={this.modalClose}
+					 	show={this.state.modalShow}
+					 	images={gridImages}
+					 	imageid={this.state.imageId}
+					 />*/}
+					{instaData.map(image => {
+						return (
+							<div className="grid-item">
+								<img
+									src={image.images.standard_resolution.url}
+									// alt={image.caption.text}
+									className="grid-image"
+									//onClick={() => this.handleShow(image.id)}
+									key={image.id}
+								/>
+							</div>
+						);
+					})}
+				</div>
+			);
+		}
 	}
 }
 
